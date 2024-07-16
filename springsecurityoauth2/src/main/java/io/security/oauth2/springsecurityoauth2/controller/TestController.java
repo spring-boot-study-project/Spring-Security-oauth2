da8a2603da7f34fa1e24be8e52c2587a07bc0350
Kang Dong Hyeon <ehdgus6435@kumoh.ac.kr>
2024-07-16 17:07:12 +0900

    chore : review test

---
+++ springsecurityoauth2/src/main/java/io/security/oauth2/springsecurityoauth2/controller/TestController.java
@@ -0,0 +1,10 @@
+package io.security.oauth2.springsecurityoauth2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    
+}
