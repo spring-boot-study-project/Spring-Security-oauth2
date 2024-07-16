74e7805c3e6170094b845387a967169789786544
Kang Dong Hyeon <ehdgus6435@kumoh.ac.kr>
2024-07-16 17:28:22 +0900

    feat : test controller 작성

--- springsecurityoauth2/src/main/java/io/security/oauth2/springsecurityoauth2/controller/TestController.java
+++ springsecurityoauth2/src/main/java/io/security/oauth2/springsecurityoauth2/controller/TestController.java
@@ -1,5 +1,6 @@
 package io.security.oauth2.springsecurityoauth2.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
     
+    @GetMapping
+    public String test() {
+        return "test";
+    }
 }
