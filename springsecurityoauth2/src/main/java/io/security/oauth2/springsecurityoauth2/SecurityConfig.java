a183003c00092b244f2df9b8f8927bb6672406b8
Kang Dong Hyeon <ehdgus6435@kumoh.ac.kr>
2024-07-24 23:29:42 +0900

    feat : spring security httpbasic 인증 방식(공부)

--- springsecurityoauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ springsecurityoauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -5,16 +5,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain defaultSecurityFilterChain1(HttpSecurity http) throws Exception {
 
         http
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated())
-                .apply(new CustomSecurityConfigurer().setFlag(false));
+                .exceptionHandling(exceptionHandlingConfigurer -> exceptionHandlingConfigurer
+                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
+
         return http.build();
     }
 }
